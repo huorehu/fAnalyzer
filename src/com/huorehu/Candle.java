cca9eed05d7285679fdbaeb21c22ee435361b32b
huorehu <ushakovser9ey@gmail.com>
2017-10-02 11:50:02 +0300

    add Candle.java, delete TicParser.java

---
+++ src/com/huorehu/Candle.java
@@ -0,0 +1,63 @@
+package com.huorehu;
+
+public class Candle {
+	
+	private static final int BEGIN_DATE_INDEX = 0;
+	private static final int END_DATE_INDEX = 10;
+	
+	private static final int BEGIN_TIME_INDEX = 11;
+	private static final int END_TIME_INDEX = 16;
+	
+	private final String date;
+	private final String time;
+	
+	private final int open;
+	private final int close;
+	private final int high;
+	private final int low;
+	
+	private final int volume;
+
+	public Candle(final String[] timeframe) {
+		this.date = timeframe[0].substring(BEGIN_DATE_INDEX, END_DATE_INDEX);
+		this.time = timeframe[0].substring(BEGIN_TIME_INDEX, END_TIME_INDEX);
+		this.open = Integer.parseInt(deleteDot(timeframe[1]));
+		this.close = Integer.parseInt(deleteDot(timeframe[2]));
+		this.high = Integer.parseInt(deleteDot(timeframe[3]));
+		this.low = Integer.parseInt(deleteDot(timeframe[4]));
+		this.volume = Integer.parseInt(timeframe[5]);
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public String getTime() {
+		return time;
+	}
+
+	public int getOpen() {
+		return open;
+	}
+
+	public int getClose() {
+		return close;
+	}
+
+	public int getHigh() {
+		return high;
+	}
+
+	public int getLow() {
+		return low;
+	}
+
+	public int getVolume() {
+		return volume;
+	}
+	
+	private String deleteDot(String str) {
+		return "1" + str.substring(2);
+	}
+	
+}
--- src/com/huorehu/ForexAnalyzer.java
+++ src/com/huorehu/ForexAnalyzer.java
@@ -2,6 +2,7 @@ package com.huorehu;
 
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Scanner;
 
 import com.opencsv.CSVReader;
 
@@ -11,8 +12,11 @@ public class ForexAnalyzer {
 		CSVReader reader = new CSVReader(new FileReader("eurusd_year.csv"));
 		String[] nextLine;
 		while((nextLine = reader.readNext()) != null) {
+			Scanner sc = new Scanner(System.in);
+			sc.nextLine();
 			if(nextLine != null) {
-				System.out.printf("Time: %s# Volume sells: %s", TicParser.getTime(nextLine[0]), nextLine[5]);
+				Candle cand = new Candle(nextLine);
+				System.out.printf("Time: %s# Volume sells: %s #%s #%d", cand.getTime(), cand.getVolume(), cand.getDate(), cand.getOpen());
 				System.out.println();
 			}
 		}
